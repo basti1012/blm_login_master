9b5432f179ed03a729d72af172ac9c3c09e1c205
basti1012 <48145852+basti1012@users.noreply.github.com>
2020-09-01 23:03:09 +0200

    Add files via upload

---
+++ install/setup-language.php
@@ -0,0 +1,39 @@
+<?php
+$setup[0]='Die Setup Datei wurde schon ausgeführt';
+$setuo[1]='Setupdatei.php exestiert noch im Ordner<br> und kann ggf noch Manuell ausgeführt werden<br>';
+$setuo[2]='Die dazugehöhrigen Setupdatein wurden vom System gelöscht<br>';
+$setup[3]='Anleitung und Setup';
+$setup[4]='Wilkommen zur vereinfachten Version eines Login Systems mit Php und Datenbank.<br>
+ 1.Starten Sie den Setup mit dem öffnen der index.php.<br>
+ Glückwunsch, das haben SIE gemacht sonst könnten SIE das hier nicht lesen.';
+ 
+$setup[5]='2.Nach erfolgreicher Installation können SIE einen Benutzer erstellen.<br>';
+$setup[26]='Das Anfangs Passwort und Adminname für den Admin Zugang sind<br>
+Adminame = 333<br>
+Password = 333<br>
+In der Admin Oberfläche können SIE den Email Text , Betreff , Absender und Header uxm ändern<br>';
+$setup[6]='3.Danach können SIE sich einloggen';
+$setup[7]='Setup starten';
+$setup[8]='Datenbank Host:';
+$setup[9]='Datenbank Name:';
+$setup[10]='Datenbank User:';
+$setup[11]='Name der Tabelle:';
+$setup[12]='Passwort:';
+$setup[13]='Link zum diesen Ordner:';
+$setup[14]='Bei unveränderter Ordnerstrucktur muß der Pfad zu diesen Ordner angeben werden ( sollte Automatisch laufen ).
+                  Falls sie einen anderen Ordner benutzen oder die passwortreset.php in einen anderen Ordner liegt , muß der Pfad zu den Ordner
+                  angegeben werden wo die Passwort wiederherstellungsdatei liegt.
+                  Dieser Link wird benötigt um den Wiederherstellungs Passwort Links in der Email zu erzeugen.';
+$setup[15]='Setupdatei Löschen ?*';
+$setup[16]='Nach erfolgreicher Installation werden die Setupdatein gelöscht.Werden in der Regel auch nicht mehr gebraucht.';
+$setup[17]='Bilder auch Löschen ?*';
+$setup[18]=' Nach erfolgreicher Installation werden auch die Bilder der Installations Anleitung gelöscht';
+$setup[19]='Es wird eine mysql.php Datei erstellt.';
+$setup[20]='Die Setupdatein werden nach erfolgreicher Installation vom Server gelöscht.';
+ $setup[21]='Installatin Login System';
+
+$setup[23]='Hier sehem sie wie der Benutzer seine Daten in sein Profil bearbeiten kann';
+$setup[24]='Als Admin ( Standart Name : 333 und Password : 333 ) können sie das ganze Tool einstellen';
+$setup[25]='Und jetzt geben sie die erforderlichen Daten an um die Installation fertig zu stellen';
+
+?>
