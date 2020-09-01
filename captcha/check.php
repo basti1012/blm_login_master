41b8fac848fa815e95fbbb3b2898eee1ad04399e
basti1012 <48145852+basti1012@users.noreply.github.com>
2020-09-01 21:17:08 +0200

    Add files via upload

---
+++ captcha/check.php
@@ -0,0 +1,9 @@
+<?php
+    session_start();
+    //print_r($_SESSION);
+    if ($_POST['captcha_code'] == $_SESSION['captcha_spam']) {
+        echo "capcha ok";
+	} else {
+		echo 'Du hast den Captcha-Code falsch eingegeben!';
+	}
+?>
---
+++ captcha/test.php
@@ -0,0 +1,5 @@
+<form method="POST" action="check.php">
+<img src="captcha.php?RELOAD=" alt="Captcha" title="Klicken, um das Captcha neu zu laden" onclick="this.src+=1;document.getElementById('captcha_code').value='';" width=140 height=40 />
+<input type="text" name="captcha_code" id="captcha_code" size=10 />
+<input type="submit">
+</form>
