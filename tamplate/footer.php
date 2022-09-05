19a00f1c6305f43f9e50df3b665c40852d350953
basti1012 <48145852+basti1012@users.noreply.github.com>
2022-09-05 21:39:54 +0200

    Update footer.php

--- tamplate/footer.php
+++ tamplate/footer.php
@@ -1,13 +1,10 @@
 <footer>
    <ul>
-   <!--   this notice may be removed   
-   you find this in tamplate/footer.php
-   -->
       <li>
-         <a href="https://soforthilfe-forum.de">do you need help ?</a>
+         <a href="https://javascript-forum.de">do you need help ?</a>
       </li>    
       <li>
-        <a href="https://sebastian1012.bplaced.net">&copy; by Basti1012</a>
+        <a href="https://basti1012.bplaced.net">&copy; by Basti1012</a>
       </li>
   </ul>
 </footer>
